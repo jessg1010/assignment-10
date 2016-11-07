5a238e09952c49fe7584e136ceb485b5788b5c5b
jessg1010 <jessicago@ufl.edu>
2016-11-07 00:44:59 -0500

    missingscripts

---
+++ scripts.js/scripts.js
@@ -0,0 +1,47 @@
+function miamiNew () {
+
+  document.getElementById("miami") .src="images/guava.jpg";
+
+}
+
+function miamiOld () {
+
+document.getElementById("miami") .src="images/miami.jpg";
+
+}
+
+function newPicture1 () {
+
+  document.getElementById("orlando") .src="images/orlandodessert.jpg";
+
+}
+
+function oldPicture1 () {
+
+document.getElementById("orlando") .src="images/orlando.jpg";
+
+}
+
+function newPicture2 () {
+
+  document.getElementById("gainesville") .src="images/miapa.jpg";
+
+}
+
+function oldPicture2 () {
+
+document.getElementById("gainesville") .src="images/gainesville.jpg";
+
+}
+
+function newPicture3 () {
+
+  document.getElementById("tallahassee") .src="tallahassee/luckygoat.jpg";
+
+}
+
+function oldPicture3 () {
+
+document.getElementById("tallahassee") .src="images/tallahassee.jpg";
+
+}
